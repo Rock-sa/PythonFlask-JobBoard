6b2cded86066d202dad64cbcccdc329ed39c48d5
Rock-sa <shakshi103103@gmail.com>
2021-05-30 17:37:05 +0530

    added route for /

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,8 @@
 #app = flask.objects(__name__)
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+@app.route('/')
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
