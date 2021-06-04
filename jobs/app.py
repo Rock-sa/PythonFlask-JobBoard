e181d6b82362984386cd67ab9748192241bb37b3
Rock-sa <shakshi103103@gmail.com>
2021-06-04 17:47:00 +0530

    base template and styling added fix

--- jobs/app.py
+++ jobs/app.py
@@ -14,7 +14,7 @@ def open_connection():
 
 def execute_sql(sql,values = (),commit = False,single = False):
     connection = open_connection()
-    cursor = execute(sql, values)
+    cursor = connection.execute(sql, values)
     if commit is True:
         results = connection.commit()
     else:
